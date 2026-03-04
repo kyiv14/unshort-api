ffd69b7d5921f52f56dd0685b61b9563c9f2fca9
kyiv14 <kyiv14@gmail.com>
2026-03-04 13:49:30 +0200

    Configure WSGI application for cPanel
    Added application path and imported wsgi_app for cPanel.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,3 +1,8 @@
 import sys, os
+
+# Указываем путь к папке приложения
 sys.path.append(os.getcwd())
+
+# Импортируем wsgi_app и переименовываем в application для cPanel
 from api import wsgi_app as application
+
