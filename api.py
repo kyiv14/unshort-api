e9813163750e742fa6cb2067120348403a9b6490
kyiv14 <kyiv14@gmail.com>
2026-03-04 13:48:44 +0200

    Update api.py

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ app = FastAPI()
 @app.get("/unmask")
 def unmask(url: str):
     try:
+        # Используем requests для надежной работы в среде cPanel
         resp = requests.head(url, allow_redirects=True, timeout=10)
         return {"original_url": url, "unmasked_url": resp.url}
     except Exception as e:
@@ -16,4 +17,5 @@ def unmask(url: str):
 def root():
     return {"status": "API is working from GitHub"}
 
+# Этот адаптер обязателен для работы FastAPI на HostIQ
 wsgi_app = ASGIMiddleware(app)
